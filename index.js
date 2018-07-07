9904ce7daf738b5f4a80de880a7433c9f0b8bf8a
Michael Boroski <mjboroski@gmail.com>
2018-07-07 19:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ let newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = randomInteger + 1000000001;
